7101637c0693f25582cedda27dd83b8b5b05028d
David Horvath <david.horvath@ibm.com>
2021-03-18 11:23:42 +0100

    completed step 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-	production: false
+	production: false,
+	githubPersonalAccessToken: '22c73e8fe4e66aa4e556a0b534685c19cce23bab'
 };
